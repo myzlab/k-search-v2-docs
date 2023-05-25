51cfcd9ee583598903e622d55d9cada1b2247517
Jean Tapias <tranx6@gmail.com>
2023-05-25 09:09:02 -0700

    Testing pwa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -167,6 +167,36 @@ const config = {
         darkTheme: darkCodeTheme,
         additionalLanguages: ["java"],
       },
+      plugins: [
+        [
+          '@docusaurus/plugin-pwa',
+          {
+            debug: true,
+            offlineModeActivationStrategies: [
+              'appInstalled',
+              'standalone',
+              'queryString',
+            ],
+            pwaHead: [
+              {
+                tagName: 'link',
+                rel: 'icon',
+                href: '/img/docusaurus.png',
+              },
+              {
+                tagName: 'link',
+                rel: 'manifest',
+                href: '/manifest.json', // your PWA manifest
+              },
+              {
+                tagName: 'meta',
+                name: 'theme-color',
+                content: 'rgb(37, 194, 160)',
+              },
+            ],
+          },
+        ],
+      ],
     }),
 };
 
